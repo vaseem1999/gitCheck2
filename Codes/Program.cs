69099725219d04d659e5bb7f454b80e6bcb89127
vaseem1999 <142472999+vaseem1999@users.noreply.github.com>
2023-08-17 11:17:01 +0530

    Single Messagecs

--- Codes/Program.cs
+++ Codes/Program.cs
@@ -1,9 +1,1 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-Console.WriteLine("Hello, World! How are you ");
-Console.WriteLine("fajf weeowf sokord ");
-Console.WriteLine(";kwhe ofeoioi oifdj");
-Console.WriteLine("asdk");
-
-Console.WriteLine("vaseem Hai");
 Console.WriteLine("Hello, World! ");
